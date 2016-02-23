9b6454875336c042a8ad06178e6f7b57ec7469e6
Steven <steventhon94@gmail.com>
2016-02-23 11:54:09 -0800

    Add functionality for req fields for country/state

--- Challenge1/script.js
+++ Challenge1/script.js
@@ -1,1 +1,22 @@
+var country, state;
 
+function setCountry() {
+  var m = document.getElementById("country");
+  manufacturer = m.options[m.selectedIndex].value;
+}
+
+function setState() {
+  var s = document.getElementById("state");
+  state = s.options[s.selectedIndex].value;
+}
+
+function submit() {
+  if (country && state) {
+    document.getElementById("msg").innerHTML = "Submitted successfully!";
+  } else {
+    document.getElementById("msg").innerHTML = "Please fill all required fields.";
+  }
+}
+
+document.getElementById("country").addEventListener("change", setCountry);
+document.getElementById("state").addEventListener("change", setState);
