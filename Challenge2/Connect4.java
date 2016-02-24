7ac827ea59ef42dce6dd2429b5a0c9e35d071362
Steven <steventhon94@gmail.com>
2016-02-23 23:23:02 -0800

    Add diagonal win checks

--- Challenge2/Connect4.java
+++ Challenge2/Connect4.java
@@ -1,129 +1,188 @@
 public class Connect4
 {
-  public final static int ROWS = 7;
-  public final static int COLS = 7;
-  private char curPlayer = 'X';
-  private char[][] board;
-  public Connect4()
-  {
-    board = new char[ROWS][COLS];
-    initBoard();
-  }
-  
-  public void initBoard()
-  {
-    for (int i = 0; i < ROWS; i++) {
-      for (int j = 0; j < COLS; j++) {
-        board[i][j] = '-';
-      }
+    public final static int ROWS = 7;
+    public final static int COLS = 7;
+    public final static int WIN = 4;
+    private char curPlayer = 'X';
+    private char[][] board;
+    public Connect4()
+    {
+        board = new char[ROWS][COLS];
+        initBoard();
     }
-  }
-  
-  public void printBoard()
-  {
-    for (int i = 0; i < ROWS; i++) {
-      for (int j = 0; j < COLS; j++) {
-        System.out.print(board[i][j]);
-      }
-      System.out.println();
-    }
-    System.out.println();
-  }
-  
-  public boolean isBoardFull()
-  {
-    boolean isFull = true;
-    for (int i = 0; i < ROWS; i++) {
-      for (int j = 0; j < COLS; j++) {
-        if (board[i][j] == '-')
-        {
-          isFull = false;
+
+    public void initBoard()
+    {
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                board[i][j] = '-';
+            }
         }
-      }
     }
-    return isFull;
-  }
-  
-  public void switchPlayer()
-  {
-    if (curPlayer == 'X')
+
+    public void printBoard()
     {
-      curPlayer = 'O';
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < ROWS; i++)
+        {
+            System.out.print(i + " ");
+        }
+        System.out.println();
     }
-    else
+
+    public boolean isBoardFull()
     {
-      curPlayer = 'X';
+        boolean isFull = true;
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                if (board[i][j] == '-')
+                {
+                    isFull = false;
+                }
+            }
+        }
+        return isFull;
     }
-  }
-  
-  public boolean canInsert(int col)
-  {
-    return board[0][col] == '-';
-  }
-  
-  public void insert(int col)
-  {
-    for (int i = 1; i < ROWS; i-- )
+
+    public void switchPlayer()
     {
-      if (board[i][col] != '-')
-      {
+        if (curPlayer == 'X')
+        {
+            curPlayer = 'O';
+        }
+        else
+        {
+            curPlayer = 'X';
+        }
+    }
+
+    public boolean canInsert(int col)
+    {
+        return board[0][col] == '-';
+    }
+
+    public void insert(int col)
+    {
+        int i;
+        for (i = 1; i < ROWS; i++)
+        {
+            if (board[i][col] != '-')
+            {
+                break;
+            }
+        }
         board[i - 1][col] = curPlayer;
-        i = ROWS;
-        switchPlayer();
-      }
     }
-  }
-  
-  public boolean checkWinRow()
-  {
-    int count = 0;
-    for (int i = 0; i < ROWS; i++) {
-      count = 0;
-      for (int j = 0; j < COLS; j++) {
-        if (board[i][j] == curPlayer)
-        {
-          count++;
+
+    public boolean checkWinRow()
+    {
+        for (int i = 0; i < ROWS; i++) {
+            int count = 0;
+            for (int j = 0; j < COLS; j++) {
+                if (board[i][j] == curPlayer)
+                {
+                    count++;
+                }
+                else
+                {
+                    count = 0;
+                }
+                if (count >= WIN)
+                {
+                    return true;
+                }
+            }
         }
-        else
-        {
-          count = 0;
-        }
-        if (count >= 4)
-        {
-          return true;
-        }
-      }
+        return false;
     }
-    return false;
-  }
-  
-  public boolean checkWinCol()
-  {
-    int count = 0;
-    for (int j = 0; j < COLS; j++) {
-      count = 0;
-      for (int i = 0; i < ROWS; j++) {
-        if (board[i][j] == curPlayer)
-        {
-          count++;
+
+    public boolean checkWinCol()
+    {
+        for (int j = 0; j < COLS; j++) {
+            int count = 0;
+            for (int i = 0; i < ROWS; i++) {
+                if (board[i][j] == curPlayer)
+                {
+                    count++;
+                }
+                else
+                {
+                    count = 0;
+                }
+                if (count >= WIN)
+                {
+                    return true;
+                }
+            }
         }
-        else
-        {
-          count = 0;
-        }
-        if (count >= 4)
-        {
-          return true;
-        }
-      }
+        return false;
     }
-    return false;
-  }
-  
-  public boolean checkWin()
-  {
-    return checkWinCol() && checkWinRow();
-        //&& checkWinDiagTopDown() && checkWinDiagBottomUp();
-  }
-  
+
+    public boolean checkWinDiagTopDown()
+    {
+        for (int j = 0; j < COLS - WIN; j++) {
+            int count = 0;
+            for (int i = 0; i < ROWS - WIN; i++) {
+                count = 0;
+                for (int k = 0; k < WIN; k++)
+                {
+                    if (board[i + k][j + k] == curPlayer)
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        count = 0;
+                    }
+                    if (count >= WIN)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    
+    public boolean checkWinDiagBottomUp()
+    {
+        for (int j = 0; j < COLS - WIN; j++) {
+            int count = 0;
+            for (int i = ROWS - 1; i > ROWS - 1 - WIN; i--) {
+                count = 0;
+                for (int k = 0; k < WIN; k++)
+                {
+                    if (board[i - k][j + k] == curPlayer)
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        count = 0;
+                    }
+                    if (count >= WIN)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean checkWin()
+    {
+        return checkWinCol() || checkWinRow() || checkWinDiagTopDown() || checkWinDiagBottomUp();
+    }
+
+    public char getCurPlayer()
+    {
+        return curPlayer;
+    }
+
 }
