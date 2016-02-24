d44942b0171cd89e1780c404d0cc068e660f3c65
Steven <steventhon94@gmail.com>
2016-02-23 22:05:24 -0800

    Create Connect4Runner.java

---
+++ Challenge2/Connect4Runner.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public Connect4Runner {
+  public static void main(String[] args)
+  {
+    Scanner s = new Scanner(System.in);
+    Connect4 game = new Connect4();
+    while (!checkWin())
+    {
+      
+    }
+  }
+}
