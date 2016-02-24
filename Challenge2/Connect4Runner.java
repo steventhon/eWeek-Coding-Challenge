beb07215b8a714921e3a0805925b14dcf5cf67fd
Steven <steventhon94@gmail.com>
2016-02-23 22:27:22 -0800

    Update game Rows

--- Challenge2/Connect4Runner.java
+++ Challenge2/Connect4Runner.java
@@ -1,13 +1,33 @@
 import java.util.*;
 
-public Connect4Runner {
-  public static void main(String[] args)
-  {
-    Scanner s = new Scanner(System.in);
-    Connect4 game = new Connect4();
-    while (!checkWin())
+public class Connect4Runner {
+    public static void main(String[] args)
     {
-      
+        Scanner s = new Scanner(System.in);
+        Connect4 game = new Connect4();
+        do
+        {
+            game.switchPlayer();
+            game.printBoard();
+            System.out.println("Enter row to insert piece (0-" + (game.ROWS - 1) +")");
+            int col = s.nextInt();
+            if (game.canInsert(col))
+            {
+                game.insert(col);
+            }
+            else
+            {
+                System.out.println("Column is full.");
+            }
+        } while (!game.checkWin() && !game.isBoardFull());
+        
+        if (game.checkWin())
+        {
+            System.out.println(game.getCurPlayer() + " wins!");
+        }
+        else
+        {
+            System.out.println("Draw!");
+        }
     }
-  }
 }
